b4cd287b6eda50d9a60ebcbe7a1ef7d7bb0d1b58
Jeevan DC <jeevan.9571@gmail.com>
2025-08-04 22:12:22 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,22 +1,32 @@
-#include "./monitor.h"
 #include "display.h"
-#include <assert.h>
 #include <iostream>
-using std::cout;
-using std::cout;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
+bool isTemperatureOk(float temperature) {
+  if (temperature < 95 || temperature > 102) {
     showBlinkingAlert("Temperature is critical!");
-    return 0;
+    return false;
   }
+  return true;
+}
+
+bool isPulseRateOk(float pulseRate) {
   if (pulseRate < 60 || pulseRate > 100) {
     showBlinkingAlert("Pulse Rate is out of range!");
-    return 0;
+    return false;
   }
+  return true;
+}
+
+bool isSpO2Ok(float spo2) {
   if (spo2 < 90) {
     showBlinkingAlert("Oxygen Saturation out of range!");
-    return 0;
+    return false;
   }
-  return 1;
+  return true;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  return isTemperatureOk(temperature) &&
+         isPulseRateOk(pulseRate) &&
+         isSpO2Ok(spo2);
 }
